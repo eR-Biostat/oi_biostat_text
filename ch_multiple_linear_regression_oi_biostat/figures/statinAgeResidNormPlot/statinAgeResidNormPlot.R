c19468eb20b67ab569ae46c4296b2864f2e3ad20
David Harrington <dph@jimmy.harvard.edu>
2018-01-01 10:33:57 -0500

    revisions to chapter 7 up to famuss example

--- ch_multiple_linear_regression_oi_biostat/figures/statinAgeResidNormPlot/statinAgeResidNormPlot.R
+++ ch_multiple_linear_regression_oi_biostat/figures/statinAgeResidNormPlot/statinAgeResidNormPlot.R
@@ -1,43 +1,39 @@
+
 library(openintro)
+#library(oibiostat)
+
 data(COL)
+#data(prevend.samp)
 
-NormalHist <- function(obs, hold, M, SD, col) {
-  plot(0, 0,
-       type = 'n',
-       xlab = 'Residuals',
-       ylab = '',
-       axes = FALSE,
-       main = '',
-       xlim = M + c(-3, 3) * SD,
-       ylim = c(0, max(hold$density)))
-  for (i in 1:length(hold$counts)) {
-    rect(hold$breaks[i], 0,
-         hold$breaks[i + 1], hold$density[i],
-         col = col)
-  }
-  axis(1)
-  x <- seq(min(obs) - 2, max(obs) + 2, 0.01)
-  y <- dnorm(x, M, SD)
-  lines(x, y, lwd = 1.5)
-}
 
-# source("famussHeightWeightHelpers.R")
 
-obs = residuals(lm(RFFT ~ Statin + Age, data=statins.samp))
-hold <- hist(obs, plot = FALSE)
+residuals = resid(lm(RFFT ~ Statin + Age, data=statins.samp))
+predicted = predict(lm(RFFT ~ Statin + Age, data=statins.samp))
 
 myPDF("statinAgeResidNormPlot.pdf", 6, 3,
       mfrow = c(1, 2),
       mgp = c(1.9, 0.5, 0),
-      mar = c(3, 0.2, 1.5, 0.8) + 0.1)
-NormalHist(obs, hold, 
-           mean(obs), 
-           sd(obs),
-           COL[1])
+      mar = c(3, 3, .5, .5) + 0.1)
+#mar = c(3, 0.2, 1.5, 0.8) + 0.1)
+plot(predicted,
+     residuals,
+     pch = 19,
+     cex = 1.3,
+     col = COL[1, 3],
+     xlab = "Predicted Value",
+     ylab = "Residual",
+     ylim = c(min(residuals), max(residuals)))
+points(residuals,
+       predicted,
+       cex = 1.3,
+       col = COL[1])
+#mtext("Predicted Value", 1, 1.9)
+
+abline(lm(residuals ~ predicted))
 
 par(mar = c(3,4,1,0))
 
-qqnorm(obs,
+qqnorm(residuals,
        cex = 0.7,
        main = '',
        axes = FALSE,
@@ -45,6 +41,6 @@ qqnorm(obs,
        col = COL[1,3])
 axis(1)
 axis(2)
-qqline(obs)
+qqline(residuals)
 
 dev.off()
