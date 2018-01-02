290b97e7e9ab658ba87753bc92723fde2219ee0e
Julie Vu <julievu777@gmail.com>
2018-01-01 19:14:59 -0500

    adding back main.tex, changing figure names to use prevend from statins

--- ch_simple_linear_regression_oi_biostat/figures/prevendResidPlot/prevendResidPlot.R
+++ ch_simple_linear_regression_oi_biostat/figures/prevendResidPlot/prevendResidPlot.R
@@ -1,21 +1,21 @@
 require(openintro)
 data(COL)
 
-myPDF('statinsResidPlot.pdf', 6.51, 3.786,
+myPDF('prevendResidPlot.pdf', 6.51, 3.786,
       mar = c(3, 3, 0.5, 1),
       mgp = c(1.8, 0.6, 0))
 these <- c(500, 170, 218)
 
-plot(137.550 - 1.261 * statins.samp$Age[-these],
-     statins.samp$RFFT[-these] - (137.550 - 1.261 * statins.samp$Age[-these]),
+plot(137.550 - 1.261 * prevend.samp$Age[-these],
+     prevend.samp$RFFT[-these] - (137.550 - 1.261 * prevend.samp$Age[-these]),
      pch = 20,
      cex = 1.5,
      col = COL[1, 3],
      xlab = 'Predicted RFFT Score',
      ylab = 'Residuals')
-y.extra <- - 1.261 * statins.samp$Age[these] + rnorm(1,0,0.01)
-points(statins.samp$Age[these] + rnorm(1, 0, 0.01),
-       statins.samp$RFFT[these] - (137.550 + y.extra),
+y.extra <- - 1.261 * prevend.samp$Age[these] + rnorm(1,0,0.01)
+points(prevend.samp$Age[these] + rnorm(1, 0, 0.01),
+       prevend.samp$RFFT[these] - (137.550 + y.extra),
        pch = c(3, 4, 2),
        col = COL[4],
        cex = 1.5,
